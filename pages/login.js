f4e11d3015b989610167fde8227fc046c9ef4234
나상엽 <gigic5220@gmail.com>
2022-12-14 21:53:23 +0900

    test

---
+++ pages/login.js
@@ -0,0 +1,100 @@
+import Head from 'next/head';
+import Router from 'next/router';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
+
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: 'demo@devias.io',
+      password: 'Password123'
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required('Password is required')
+    }),
+    onSubmit: () => {
+      Router
+        .push('/')
+        .catch(console.error);
+    }
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Login | Material Kit</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%'
+        }}
+      >
+        <Container maxWidth="sm">
+          <form onSubmit={formik.handleSubmit}>
+            <Box sx={{ my: 3, mb: 10 }}>
+              <Typography
+                color="textPrimary"
+                variant="h4"
+              >
+                관리자 로그인
+              </Typography>
+            </Box>
+            <TextField
+              error={Boolean(formik.touched.email && formik.errors.email)}
+              fullWidth
+              helperText={formik.touched.email && formik.errors.email}
+              label="관리자 계정"
+              margin="normal"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              value={formik.values.email}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.password && formik.errors.password)}
+              fullWidth
+              helperText={formik.touched.password && formik.errors.password}
+              label="비밀번호"
+              margin="normal"
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="password"
+              value={formik.values.password}
+              variant="outlined"
+            />
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                disabled={formik.isSubmitting}
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                로그인
+              </Button>
+            </Box>
+          </form>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Login;
